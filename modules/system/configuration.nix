a72fe2f97b5b64f8b28f1254a4378f97b2339a81
maxim <mxmclaeys@gmail.com>
2024-02-25 16:19:58 +0100

    hyprland test

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -12,7 +12,7 @@
   programs.zsh.enable = true;
   programs.hyprland ={
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    #package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 
   environment.systemPackages = with pkgs; [
