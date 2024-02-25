fcecb1008b7c49eee05af73aad0fe79bd2ab3f84
maxim <mxmclaeys@gmail.com>
2024-02-25 15:51:03 +0100

    hyprland changes

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -6,11 +6,6 @@ let cfg = config.modules.hyprland;
 in {
     options.modules.hyprland= { enable = mkEnableOption "hyprland"; };
     config = mkIf cfg.enable {
-
-    programs.hyprland ={
-        enable = true;
-        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    };
     
 	home.packages = with pkgs; [
 	    wofi swaybg wlsunset wl-clipboard hyprland
--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -10,6 +10,10 @@
   services.xserver.desktopManager.xterm.enable = false;
 
   programs.zsh.enable = true;
+  programs.hyprland ={
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
 
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
